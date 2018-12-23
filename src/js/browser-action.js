47b4b33b958b686717b71bc2dc4f80b14b8ef440
jossef <jossef12@gmail.com>
2018-12-23 19:36:01 +0200

    tidy up, fixing breaking changes, adding exclusion for deleted answers
    close #3
    close #2

--- src/js/browser-action.js
+++ src/js/browser-action.js
@@ -1,34 +1,34 @@
 $(function () {
-     chrome.storage.sync.get({
-       enableSortByMyUpvotes: true,
-       enableSortByMyAnswers: true,
-       enableHighlightByMyAnswers: true,
-     }, function(items) {
-       $('#enable-sort-by-my-upvotes').prop('checked', items.enableSortByMyUpvotes)
-       $('#enable-sort-by-my-answers').prop('checked', items.enableSortByMyAnswers)
-       $('#enable-highlight-by-my-answers').prop('checked', items.enableHighlightByMyAnswers)
-     });
+    chrome.storage.sync.get({
+        enableSortByMyUpvotes: true,
+        enableSortByMyAnswers: true,
+        enableHighlightByMyAnswers: true,
+    }, function (items) {
+        $('#enable-sort-by-my-upvotes').prop('checked', items.enableSortByMyUpvotes);
+        $('#enable-sort-by-my-answers').prop('checked', items.enableSortByMyAnswers);
+        $('#enable-highlight-by-my-answers').prop('checked', items.enableHighlightByMyAnswers);
+    });
 
-     $('#enable-sort-by-my-upvotes').change(function(){
-       chrome.storage.sync.set({
-         enableSortByMyUpvotes: $('#enable-sort-by-my-upvotes').prop('checked')
-        }, function() {
+    $('#enable-sort-by-my-upvotes').change(function () {
+        chrome.storage.sync.set({
+            enableSortByMyUpvotes: $('#enable-sort-by-my-upvotes').prop('checked')
+        }, function () {
         });
-     })
+    });
 
-    $('#enable-sort-by-my-answers').change(function(){
-      chrome.storage.sync.set({
-        enableSortByMyAnswers: $('#enable-sort-by-my-answers').prop('checked')
-       }, function() {
-       });
-    })
-
-     $('#enable-highlight-by-my-answers').change(function(){
-       chrome.storage.sync.set({
-         enableHighlightByMyAnswers: $('#enable-highlight-by-my-answers').prop('checked')
-        }, function() {
+    $('#enable-sort-by-my-answers').change(function () {
+        chrome.storage.sync.set({
+            enableSortByMyAnswers: $('#enable-sort-by-my-answers').prop('checked')
+        }, function () {
         });
-     })
+    });
+
+    $('#enable-highlight-by-my-answers').change(function () {
+        chrome.storage.sync.set({
+            enableHighlightByMyAnswers: $('#enable-highlight-by-my-answers').prop('checked')
+        }, function () {
+        });
+    });
 
     $('.link').click(function () {
         var address = $(this).attr('data-href');
--- src/js/content-script.js
+++ src/js/content-script.js
@@ -1,167 +1,165 @@
-jQuery(function(){
+jQuery(function () {
 
-  chrome.storage.sync.get({
-    enableSortByMyUpvotes: true,
-    enableSortByMyAnswers: true,
-    enableHighlightByMyAnswers: true,
-  }, function(options) {
+    chrome.storage.sync.get({
+        enableSortByMyUpvotes: true,
+        enableSortByMyAnswers: true,
+        enableHighlightByMyAnswers: true,
+    }, function (options) {
 
-var isDisabled = !options.enableSortByMyUpvotes &&
-  !options.enableSortByMyAnswers &&
-  !options.enableHighlightByMyAnswers;
+        var isDisabled = !options.enableSortByMyUpvotes &&
+            !options.enableSortByMyAnswers &&
+            !options.enableHighlightByMyAnswers;
 
-    if (isDisabled)
-    {
-      return;
-    }
+        if (isDisabled) {
+            return;
+        }
 
-    var answers = getAnswers(options);
-    sortAnswers(answers);
+        var answers = getAnswers(options);
+        sortAnswers(answers);
 
-    if (options.enableHighlightByMyAnswers){
+        if (options.enableHighlightByMyAnswers) {
 
-      // workaround;
-      options.enableSortByMyAnswers = true;
-      answers = getAnswers(options);
-      // end of workaround
+            // workaround;
+            options.enableSortByMyAnswers = true;
+            answers = getAnswers(options);
+            // end of workaround
 
-      markAnswersAsUpVoted(answers.my);
-    }
+            markAnswersAsUpVoted(answers.my);
+        }
 
-    // scroll back to ancor element
-    var hash = window.location.hash && window.location.hash.substring(1);
-    if (hash){
-      var ancorElement = document.getElementById('answer-' + hash);
-      if (ancorElement){
+        // scroll back to ancor element
+        var hash = window.location.hash && window.location.hash.substring(1);
+        if (hash) {
+            var ancorElement = document.getElementById('answer-' + hash);
+            if (ancorElement) {
 
-        setTimeout(function(){
-          jQuery('html, body').animate({
-              scrollTop: jQuery(ancorElement).offset().top
-          }, 0);
-        }, 300);
-      }
-    }
-  });
+                setTimeout(function () {
+                    jQuery('html, body').animate({
+                        scrollTop: jQuery(ancorElement).offset().top
+                    }, 0);
+                }, 300);
+            }
+        }
+    });
 
 
-  function calculateAnswerPriorities(answers){
-    var answerPriorities = {};
+    function calculateAnswerPriorities(answers) {
+        var answerPriorities = {};
 
-    var sortedAnswers = [
-        answers.my,
-        answers.upVoted,
-        answers.neutral,
-        answers.downVoted
-      ];
+        var sortedAnswers = [
+            answers.my,
+            answers.upVoted,
+            answers.neutral,
+            answers.downVoted
+        ];
 
-    var position = 0;
+        var position = 0;
 
-    sortedAnswers.forEach(function(subAnswers) {
-        subAnswers.forEach(function(answer) {
-          var answerId = jQuery(answer).attr('id');
-          answerPriorities[answerId] = position++;
+        sortedAnswers.forEach(function (subAnswers) {
+            subAnswers.forEach(function (answer) {
+                var answerId = jQuery(answer).attr('id');
+                answerPriorities[answerId] = position++;
+            });
         });
-    });
 
-    return answerPriorities;
-  }
-
-  function sortAnswers(answers){
-
-    var answerPriorities = calculateAnswerPriorities(answers);
-    var allAnswers = answers.all.slice();
-
-    // Bubble sorting the answers
-    var swapped = true;
-    var lastUnsortedIndex = allAnswers.length;
-    while(swapped){
-      var swapped = false;
-      for (var i = 0; i < lastUnsortedIndex - 1; i++){
-
-        var leftAnswer = allAnswers[i];
-        var leftAnswerId = jQuery(leftAnswer).attr('id');
-
-        var rightAnswer = allAnswers[i + 1];
-        var rightAnswerId = jQuery(rightAnswer).attr('id');
-
-
-        if (answerPriorities[leftAnswerId] > answerPriorities[rightAnswerId])
-        {
-              allAnswers[i] = rightAnswer;
-              allAnswers[i + 1] = leftAnswer;
-              swapElements(leftAnswer, rightAnswer);
-              swapped = true;
-        }
-      }
-
-      lastUnsortedIndex -= 1;
+        return answerPriorities;
     }
 
-  }
+    function sortAnswers(answers) {
 
-  function swapElements(first, second) {
-    if (first == second || !first || !second){
-      return;
+        var answerPriorities = calculateAnswerPriorities(answers);
+        var allAnswers = answers.all.slice();
+
+        // Bubble sorting the answers
+        var swapped = true;
+        var lastUnsortedIndex = allAnswers.length;
+        while (swapped) {
+            var swapped = false;
+            for (var i = 0; i < lastUnsortedIndex - 1; i++) {
+
+                var leftAnswer = allAnswers[i];
+                var leftAnswerId = jQuery(leftAnswer).attr('id');
+
+                var rightAnswer = allAnswers[i + 1];
+                var rightAnswerId = jQuery(rightAnswer).attr('id');
+
+
+                if (answerPriorities[leftAnswerId] > answerPriorities[rightAnswerId]) {
+                    allAnswers[i] = rightAnswer;
+                    allAnswers[i + 1] = leftAnswer;
+                    swapElements(leftAnswer, rightAnswer);
+                    swapped = true;
+                }
+            }
+
+            lastUnsortedIndex -= 1;
+        }
+
     }
 
-    first = jQuery(first);
-    second = jQuery(second);
-    var temp = jQuery('<span>').hide();
-    first.before(temp);
-    second.before(first);
-    temp.replaceWith(second);
-  };
-
-  function getAnswers(options){
-    var my = [];
-    var upVoted = [];
-    var neutral = [];
-    var downVoted = [];
-    var all = jQuery('.answer').toArray();
-
-    var loggedUserProfileLink = jQuery('.my-profile,.profile-me').slice(0,1).attr('href');
-
-    all.forEach(function(answer, position) {
-        answer = jQuery(answer);
-
-        var answerUserProfileLink = answer.find('.user-details > a').last().attr('href');
-
-        // Up vote
-        if (options.enableSortByMyUpvotes && answer.find('.vote-up-on').length) {
-          upVoted.push(answer);
+    function swapElements(first, second) {
+        if (first == second || !first || !second) {
+            return;
         }
 
-        // Down vote
-         else if (options.enableSortByMyUpvotes && answer.find('.vote-down-on').length) {
-          downVoted.push(answer);
-        }
+        first = jQuery(first);
+        second = jQuery(second);
+        var temp = jQuery('<span>').hide();
+        first.before(temp);
+        second.before(first);
+        temp.replaceWith(second);
+    };
 
-        // Current logged in user's answer
-        else if (options.enableSortByMyAnswers && answerUserProfileLink && loggedUserProfileLink == answerUserProfileLink){
-            my.push(answer);
-        }
-        // Neutral - did not vote
-        else{
-          neutral.push(answer);
-        }
+    function getAnswers(options) {
+        var my = [];
+        var upVoted = [];
+        var neutral = [];
+        var downVoted = [];
+        var all = jQuery('.answer:not(.deleted-answer)').toArray();
 
-      });
+        var loggedUserProfileLink = jQuery('.my-profile,.profile-me').slice(0, 1).attr('href');
 
-    return {
-        my: my,
-        upVoted: upVoted,
-        neutral: neutral,
-        downVoted: downVoted,
-        all:  all
-      };
-  }
+        all.forEach(function (answer, position) {
+            answer = jQuery(answer);
 
-  function markAnswersAsUpVoted(answers){
-    answers.forEach(function(answer) {
-      answer = jQuery(answer);
-      answer.find('.vote-up-off').addClass('vote-up-on');
-    });
-  }
+            var answerUserProfileLink = answer.find('.user-details > a').last().attr('href');
+
+            // Up vote
+            if (options.enableSortByMyUpvotes && answer.find('.js-vote-up-btn.fc-theme-primary').length) {
+                upVoted.push(answer);
+            }
+
+            // Down vote
+            else if (options.enableSortByMyUpvotes && answer.find('.js-vote-down-btn.fc-theme-primary').length) {
+                downVoted.push(answer);
+            }
+
+            // Current logged in user's answer
+            else if (options.enableSortByMyAnswers && answerUserProfileLink && loggedUserProfileLink == answerUserProfileLink) {
+                my.push(answer);
+            }
+            // Neutral - did not vote
+            else {
+                neutral.push(answer);
+            }
+
+        });
+
+        return {
+            my: my,
+            upVoted: upVoted,
+            neutral: neutral,
+            downVoted: downVoted,
+            all: all
+        };
+    }
+
+    function markAnswersAsUpVoted(answers) {
+        answers.forEach(function (answer) {
+            answer = jQuery(answer);
+            answer.find('.js-vote-up-btn').addClass('fc-theme-primary');
+        });
+    }
 
 
 });
